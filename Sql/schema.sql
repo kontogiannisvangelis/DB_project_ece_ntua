7a1aecfff85b0b8b25da99a873d0ab5fc7246441
Theodora Boutsini <boutsinidora@gmail.com>
2022-06-02 21:55:12 +0300

    update triggers

--- Sql/schema.sql
+++ Sql/schema.sql
@@ -207,6 +207,19 @@ signal sqlstate '45000';
 end if;
 end$$
 DELIMITER ;
+DELIMITER $$
+create trigger evaluator_check_up before update on Evaluation
+for each row
+begin 
+if new.Organization_id = (
+		select r.Organization_id
+        from Researcher r
+        where new.Researcher_id = r.Researcher_id)
+then
+signal sqlstate '45000';
+end if;
+end$$
+DELIMITER ;
 
 
 DELIMITER $$
@@ -234,7 +247,6 @@ for each row
 begin 
 if (select TIMESTAMPDIFF(Year, new.Start_date, new.End_Date)) not in (1,2,3,4)
 or ((new.amount is not null and (select e.Grade from Evaluation e order by Evaluation_id Desc limit 1) < 50))
-or new.Organization_id in (select r.Organization_id from Researcher r where r.Researcher_id = new.Researcher_id)
 then
 signal sqlstate '45000';
 end if;
@@ -245,9 +257,9 @@ for each row
 begin 
 if (select TIMESTAMPDIFF(Year, new.Start_date, new.End_Date)) not in (1,2,3,4)
 or ((new.amount is not null and (select e.Grade from Evaluation e order by Evaluation_id Desc limit 1) < 50))
-or new.Organization_id in (select r.Organization_id from Researcher r where r.Researcher_id = new.Researcher_id)
 then
 signal sqlstate '45000';
+update Evaluation set Organization_id = new.Organization_id;
 end if;
 end$$
 
